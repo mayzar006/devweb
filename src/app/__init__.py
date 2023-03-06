4e7a09004ab7a9d370007092de7d1995c2253b2d
mayzar006 <101275071+mayzar006@users.noreply.github.com>
2023-03-06 17:50:24 +0100

    Add files via upload

---
+++ src/app/__init__.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import os
+from sqlalchemy import select
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db=SQLAlchemy(app)
+#db.init_app(app)
+
+class Data(db.Model):
+    __tablename__ = "data"
+
+    id = db.Column(db.Integer, primary_key=True)
+    rna_id = db.Column(db.String(30), nullable=True) 
+    rna_id_ex = db.Column(db.String(30), nullable=True)
+    gestion = db.Column(db.String(20), nullable=True)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/assos')
+def assos():
+    datas = Data.query.limit(30).all()
+    return render_template('assos.html', datas=datas)
+
+@app.route('/ajouter', methods=['GET', 'POST'])
+def ajouter():
+    if request.method == 'POST':
+        rna_id = request.form['rna_id']
+        rna_id_ex = request.form['rna_id_ex']
+        gestion = request.form['gestion']
+        new_data = Data(rna_id=rna_id, rna_id_ex=rna_id_ex, gestion=gestion)
+        db.session.add(new_data)
+        db.session.commit()
+        return redirect(url_for('assos'))
+    return render_template('ajouter.html')
+
+@app.route('/modifier/<int:data_id>', methods=['GET', 'POST'])
+def modifier(data_id):
+    data = Data.query.get(data_id)
+
+    if request.method == 'POST':
+        data.rna_id = request.form['rna_id']
+        data.rna_id_ex = request.form['rna_id_ex']
+        data.gestion = request.form['gestion']
+        db.session.commit()
+        return redirect(url_for('assos'))
+    return render_template('modifier.html', data=data)
+
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+if __name__ == '__main__':
+    app.run()  
+
